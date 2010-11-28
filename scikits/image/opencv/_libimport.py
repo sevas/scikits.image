d601b9725a2892f483b1834f57be0f3618b57525
sevas <f.degroef@gmail.com>
2010-11-28 01:24:32 +0100

    better detection of dll names on windows

--- scikits/image/opencv/_libimport.py
+++ scikits/image/opencv/_libimport.py
@@ -17,6 +17,45 @@ import sys
 import os.path
 import warnings
 
+
+def _extract_windows_prefix_and_version(cxcore_dll_name):
+    """
+    Extract the prefix and the version strings, from the cxcore DLL name. 
+            
+    Example: 
+        - with the compiled OpenCV2.0 binaries
+        libcxcore200.dll -> returns ("lib", "200")
+        
+        - with the compiled OpenCV2.1 binaries
+        cxcore210.dll -> returns ("", "210")
+    """
+    prefix, version = cxcore_dll_name.split("cxcore")
+    return prefix, version
+
+
+
+def _detect_windows_opencv_prefix_and_version():
+    """
+    Checks the windows %PATH% and tries to guess the naming scheme of 
+    the installed OpenCV 2.x binaries.
+    """
+    import os
+    windows_path = os.getenv("PATH").split(";")
+    opencv_paths = [s for s in windows_path if s.lower().find("opencv")!=-1]
+    if not opencv_paths:
+        warning.warn("No OpenCV found in path. Please install OpenCV >= 2.0")
+    opencv_bin_path = opencv_paths[0]
+    opencv_release_dlls = [f for f in os.listdir(opencv_bin_path) if 
+                                    f.endswith(".dll") and 
+                                    not f.endswith("d.dll")]
+    
+    # we use cxcore as reference because it's not an optionnal library, 
+    # and the substring "cv" is found in both cv and cvaux
+    cxcore_dll_name = [f for f in opencv_release_dlls if f.find("cxcore") != -1][0]    
+    return _extract_windows_prefix_and_version(cxcore_dll_name.rstrip(".dll"))
+
+
+
 def _import_opencv_lib(which="cv"):
     """
     Try to import a shared library of OpenCV.
@@ -31,15 +70,19 @@ def _import_opencv_lib(which="cv"):
                      '/sw/lib/', # Fink
                      ]
 
+    lib, version = ("", "")
     if sys.platform.startswith('linux'):
         extensions = ['.so', '.so.1']
+        lib = 'lib' + which
     elif sys.platform.startswith("darwin"):
         extensions = ['.dylib']
+        lib = 'lib' + which
     else:
         extensions = ['.dll']
+        prefix, version = _detect_windows_opencv_prefix_and_version()
+        lib = prefix + which + version
         library_paths = ['']
 
-    lib = 'lib' + which
     shared_lib = None
 
     for path in library_paths:
